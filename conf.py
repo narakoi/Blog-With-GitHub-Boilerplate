650f605bc2d78668288048c22bf612348295d5a9
koi <62449657+narakoi@users.noreply.github.com>
2020-03-21 07:52:47 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ enable_jsdelivr = {
 
 # 站点设置
 site_name = "Escape Project"
-site_logo = "${static_prefix}logo.png"
+site_logo = "${static_prefix}logo-头像.jpg"
 site_build_date = "2019-12-18T16:51+08:00"
 author = "KOI"
 email = "narakyzlily@gmail.com"
